700d0dfe554f34280a509fa0a085244a854188e3
yy_yank <yy.yank.dev.blog@gmail.com>
2016-07-28 22:25:53 +0900

    hello syakyou

---
+++ src/main/java/HelloSyakyou.java
@@ -0,0 +1,9 @@
+class HelloSyakyo {
+    public static void main(String[] args) {
+        System.out.println(
+        "=====================" + System.lineSeparator()
+        +"|　Hello Syakyou!!!|" + System.lineSeparator()
+        +"===================="+ System.lineSeparator()
+        );
+    }
+}
